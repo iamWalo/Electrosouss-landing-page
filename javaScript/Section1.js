894fcc300e390556498a9fa8a9f0ec6e320d5dbd
Yanis <developeryanis@gmail.com>
2024-12-12 14:15:16 +0000

    first commit

--- javaScript/Section1.js
+++ javaScript/Section1.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-
-
-const liLink = document.querySelector('.sidebar')
+const liLink = document.querySelector('.sidebar');
 const sideBar = document.querySelector('.sidebar');
 liLink.addEventListener('click', () => {
     const showDiv = document.createElement('div');
@@ -45,54 +42,5 @@ const showSidebar = () => {
         sideBar.style.display = 'none';
     } else {
         sideBar.style.display = 'block'
-    }
-
-
-
-    const liLink = document.querySelector('.sidebar')
-    const sideBar = document.querySelector('.sidebar');
-    liLink.addEventListener('click', () => {
-        const showDiv = document.createElement('div');
-        showDiv.className = 'show-div'
-        showDiv.innerHTML = `
-         <ul>
-                    <li>
-                        <i class="fa-solid fa-blender"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-truck-fast"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-shower"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-cart-shopping"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-bed"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-tty"></i><span>Lorem ipsum dolor sit.</span>
-                    </li>
-                </ul>
-        `;
-
-        if (sideBar.children[1]) {
-            sideBar.removeChild(sideBar.children[1]);
-        } else {
-            sideBar.appendChild(showDiv);
-        }
-
-    })
-
-    console.log('hello world');
-
-
-    const showSidebar = () => {
-        if (sideBar.style.display === 'block') {
-            sideBar.style.display = 'none';
-        } else {
-            sideBar.style.display = 'block'
-        }
     }
 }
